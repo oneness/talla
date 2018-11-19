72509ba4a7fa76dfe9a61d748fc0e4746a7c7e76
Kasim Tuman <ktuman@gmail.com>
2018-11-18 21:21:11 -0800

    Added simple component and props

--- src/main/talla/ui/root.cljs
+++ src/main/talla/ui/root.cljs
@@ -4,15 +4,23 @@
     [fulcro.client.primitives :as prim :refer [defsc]]
     [talla.ui.components :as comp]))
 
-(defsc Root [this {:keys [root/message]}]
-  {:query         [:root/message]
-   :initial-state {:root/message "Hello!"}}
+(defsc Person [this {:keys [person/name person/profession]}]
+  (dom/div
+    (dom/p "Name: " name)
+    (dom/p "Profession: " profession)))
+
+(def ui-person (prim/factory Person))
+
+(defsc Root [this {:keys [root/message person/name person/profession]}]
+  {:query         [:root/message :person/name :person/profession]
+   :initial-state {:root/message "Hello!" :person/name "Kasim" :person/profession "Developer"}}
   (div :.ui.segments
     (div :.ui.top.attached.segment
       (div :.content
-        "Welcome to Fulcro!"))
+        (str "Welcome to Fulcro! " name)))
     (div :.ui.attached.segment
       (div :.content
         (comp/ui-placeholder {:w 50 :h 50})
-        (div message)
+        (div (str message " to " name))
+        (ui-person {:person/name name, :person/profession profession})
         (div "Some content here would be nice.")))))
